1984074fd224899871c1aa8a4ca2f46215dd387d
komle <k_nastya3@mail.ru>
2022-01-10 14:43:59 +0500

    routes on map

--- app/src/main/java/com/example/stenograffia/ui/boughtRoute/BoughtRouteFragment.kt
+++ app/src/main/java/com/example/stenograffia/ui/boughtRoute/BoughtRouteFragment.kt
@@ -2,35 +2,27 @@ package com.example.stenograffia.ui.boughtRoute
 
 import android.annotation.SuppressLint
 import android.content.ContextWrapper
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.nfc.Tag
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import com.canhub.cropper.options
 import com.example.stenograffia.R
-import com.example.stenograffia.retrofit.ApiInterface
-import com.example.stenograffia.retrofit.Result
-import com.example.stenograffia.retrofit.RouteForRetrofit
 import com.example.stenograffia.ui.data.Models.Point
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
-import io.reactivex.SingleObserver
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.internal.operators.single.SingleObserveOn
-import io.reactivex.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class BoughtRouteFragment : Fragment(), GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
@@ -38,11 +30,15 @@ class BoughtRouteFragment : Fragment(), GoogleMap.OnMarkerClickListener, OnMapRe
     lateinit var map: SupportMapFragment
     lateinit var googleMapLate: GoogleMap
     private var idRoute: String = ""
-    lateinit var apiInterface: ApiInterface
-    private lateinit var polyLineList: List<LatLng>
-    private lateinit var polylineOptions: PolylineOptions
-    private lateinit var origion: LatLng
-    private lateinit var dest: LatLng
+
+    private var originLatitude: Double = 56.901685
+    private var originLongitude: Double = 60.638509
+    private var destinationLatitude: Double = 56.788791
+    private var destinationLongitude: Double = 60.647781
+
+    private var origin: LatLng = LatLng(0.0, 0.0)
+    private var destination: LatLng = LatLng(0.0, 0.0)
+    private var waypoints: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,8 +56,42 @@ class BoughtRouteFragment : Fragment(), GoogleMap.OnMarkerClickListener, OnMapRe
             BoughtRouteViewModel::class.java
         )
 
+        val ai: ApplicationInfo = requireContext().packageManager
+            .getApplicationInfo(requireContext().packageName, PackageManager.GET_META_DATA)
+        val value = ai.metaData["com.google.android.geo.API_KEY"]
+        val apiKey = value.toString()
+
         map.onCreate(savedInstanceState)
         map.getMapAsync(this)
+
+        map.getMapAsync {
+            googleMapLate = it
+            val ekbBounds = LatLngBounds(
+                LatLng((56.821496), 60.578646),// SW bounds
+                LatLng((56.892695), 60.652224)// NE bounds
+            )
+            googleMapLate.moveCamera(CameraUpdateFactory.newLatLngZoom(ekbBounds.center, 14f))
+            googleMapLate.setOnMarkerClickListener(this)
+
+            boughtRouteViewModel.placesId.observe(viewLifecycleOwner, Observer { points ->
+                for (id in 0 until points.size) {
+                    if (id == 0) origin = LatLng(points[id]!!.latitude.toDouble(), points[id]!!.longitude.toDouble())
+                    else if (id == points.size - 1) destination = LatLng(points[id]!!.latitude.toDouble(), points[id]!!.longitude.toDouble())
+                    else if (id == points.size - 2) waypoints+="${points[id]!!.latitude},${points[id]!!.longitude}"
+                    else waypoints+="${points[id]!!.latitude},${points[id]!!.longitude}|"
+
+                    val marker = LatLng(points[id]!!.latitude.toDouble(), points[id]!!.longitude.toDouble())
+
+                    googleMapLate.addMarker(
+                        MarkerOptions()
+                            .position(marker)
+                    )
+                    val urll = getDirectionURL(origin, destination, waypoints, apiKey)
+                    GetDirection(urll).execute()
+                    googleMapLate.animateCamera(CameraUpdateFactory.newLatLngZoom(origin, 14F))
+                }
+            })
+        }
 
 //        map.getMapAsync(OnMapReadyCallback {
 //
@@ -84,16 +114,57 @@ class BoughtRouteFragment : Fragment(), GoogleMap.OnMarkerClickListener, OnMapRe
 //            })
 //        })
 
-        val retrofit: Retrofit =
-            Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .baseUrl("https://maps.googleapis.com/")
-                .build()
-
-        apiInterface = retrofit.create(ApiInterface::class.java)
         return root
     }
 
+    private fun getDirectionURL(origin:LatLng, dest:LatLng, waypoints: String, secret: String) : String{
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${dest.latitude},${dest.longitude}&waypoints=$waypoints&mode=walking&key=$secret"
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private inner class GetDirection(val url : String) : AsyncTask<Void, Void, List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body()!!.string()
+
+            val result =  ArrayList<List<LatLng>>()
+            try{
+                val respObj = Gson().fromJson(data,MapData::class.java)
+                val path =  ArrayList<LatLng>()
+                for (n in 0 until respObj.routes[0].legs.size){
+                    for (i in 0 until respObj.routes[0].legs[n].steps.size){
+                        path.addAll(decodePolyline(respObj.routes[0].legs[n].steps[i].polyline.points))
+                    }
+                }
+
+                result.add(path)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.BLUE)
+                lineoption.geodesic(true)
+            }
+            googleMapLate.addPolyline(lineoption)
+        }
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        googleMapLate = p0!!
+        val originLocation = LatLng(origin.latitude, origin.longitude)
+        googleMapLate.clear()
+        googleMapLate.addMarker(MarkerOptions().position(originLocation))
+        googleMapLate.animateCamera(CameraUpdateFactory.newLatLngZoom(originLocation, 18F))
+    }
     override fun onMarkerClick(marker: Marker): Boolean {
         boughtRouteViewModel.placesId.observe(viewLifecycleOwner, Observer {
             val bundle = Bundle()
@@ -115,64 +186,6 @@ class BoughtRouteFragment : Fragment(), GoogleMap.OnMarkerClickListener, OnMapRe
             view?.findNavController()?.navigate(R.id.routeGuideFragment, bundle)
         })
         return false
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        googleMapLate = googleMap
-        googleMapLate.mapType = GoogleMap.MAP_TYPE_NORMAL
-        googleMapLate.isTrafficEnabled = false
-        origion = LatLng(56.901685,60.638509)
-        dest = LatLng(56.788791,60.647781)
-
-        googleMapLate.addMarker(MarkerOptions().position(origion))
-        googleMapLate.addMarker(MarkerOptions().position(dest))
-
-        getDirection("56.901685" + "," + "60.638509", "56.788791" + "," + "60.647781")
-    }
-
-    @SuppressLint("CheckResult")
-    private fun getDirection(origin: String, destination: String) {
-        apiInterface.getDirection(
-            "driving",
-            "less_driving",
-            origin,
-            destination,
-            getString(R.string.api_key)
-        ).subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : SingleObserver<Result> {
-                override fun onSubscribe(d: Disposable) {
-
-                }
-
-                override fun onSuccess(t: Result) {
-                    polyLineList = ArrayList()
-                    val routeList: List<RouteForRetrofit> = t.getRoutes()
-                    Toast.makeText(context,t.toString(),Toast.LENGTH_LONG).show()
-                    for (route: RouteForRetrofit in routeList) {
-                        val polyLine: String = route.getOverviewPolyline().getPoints()
-                        (polyLineList as ArrayList<LatLng>).addAll(decodePolyline(polyLine))
-                    }
-//                    Log.d("PLEASE",polyLineList[0].toString())
-                    polylineOptions = PolylineOptions()
-                    polylineOptions.color(Color.BLUE)
-                    polylineOptions.width(8F)
-                    polylineOptions.startCap(ButtCap())
-                    polylineOptions.jointType(JointType.ROUND)
-                    polylineOptions.addAll(polyLineList)
-                    googleMapLate.addPolyline(polylineOptions)
-
-                    val builder: LatLngBounds.Builder = LatLngBounds.Builder()
-                    builder.include(origion)
-                    builder.include(dest)
-                    googleMapLate.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 100))
-                }
-
-                override fun onError(e: Throwable) {
-
-                }
-
-            })
     }
 
     fun decodePolyline(encoded: String): List<LatLng> {
@@ -206,4 +219,5 @@ class BoughtRouteFragment : Fragment(), GoogleMap.OnMarkerClickListener, OnMapRe
         }
         return poly
     }
+
 }
